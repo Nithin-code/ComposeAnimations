081c89c736306b5ad6793dc3e4848d3dad1fda2d
Nithin Kondeti <shanmukhsainithin@gmail.com>
2025-04-13 20:01:53 +0530

    Added Basic Animations

--- app/src/main/java/com/nithin/composeanimations/MainActivity.kt
+++ app/src/main/java/com/nithin/composeanimations/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.nithin.composeanimations.animations.basicanimations.modifier.AnimateBothAxesScreen
+import com.nithin.composeanimations.animations.basicanimations.modifier.AnimateYOffsetScreen
+import com.nithin.composeanimations.animations.basicanimations.modifier.CardFlipAnimation
+import com.nithin.composeanimations.animations.basicanimations.modifier.OverlappingScreen1
+import com.nithin.composeanimations.animations.basicanimations.modifier.PeekAndPopEffectScreen
+import com.nithin.composeanimations.animations.basicanimations.modifier.SlidingCardScreen
+import com.nithin.composeanimations.animations.basicanimations.modifier.SlidingDrawerScreen
 import com.nithin.composeanimations.ui.theme.ComposeAnimationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +31,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeAnimationsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    CardFlipAnimation(innerPadding)
+//                    Box(
+//                        modifier = Modifier.
+//                        fillMaxSize().
+//                        padding(innerPadding)
+//                            .background(color = Color.Black)
+//                    ) {
+//                        SlidingCardScreen()
+//                    }
                 }
             }
         }
